49c8d68d5eaec1aa51ad93dbf3215a34a4261f3e
yuanyifei <yuanp0813@163.com>
2018-03-08 22:01:39 +0800

    双层胡牌链

--- zigong_majiang/ai/test.py
+++ zigong_majiang/ai/test.py
@@ -5,4 +5,4 @@ from zigong_majiang.rule.tile_convert import TilesConverter
 tiles_18 = TilesConverter.string_to_18_array(tongzi="112345679", tiaozi="1134")
 print(Hands(tiles_18))
 ves = VesAI(2)
-ves.calc_effective_cards(tiles_18,1,[])
+ves.calc_effective_cards(tiles_18, 2)
--- zigong_majiang/ai/ves_ai.py
+++ zigong_majiang/ai/ves_ai.py
@@ -22,14 +22,16 @@ class VesAI(object):
                 tiles_18[card] -= 1
         return draw_hands
 
-    def calc_effective_cards(self, tiles_18, n, tpps):
-        # 听牌
-        draw_hands = self.calc_draw_hands(tiles_18)
-        for draw_hand in draw_hands:
-            print("听牌:", Tile(draw_hand).__str__())
+    def calc_effective_cards(self, tiles_18, n):
+        judge_tile_chains = self.calc_effective_cards_internal(tiles_18, n)
+        # for chain in judge_tile_chains:
+        #     print(chain)
 
-        # 一层胡牌链
-        first_layer_jtc = []
+    def calc_effective_cards_internal(self, tiles_18, n, judge_tile_chains=[], touch_play_pairs=[]):
+        draw_hands_cur = self.calc_draw_hands(tiles_18)
+        if draw_hands_cur:
+            judge_tile_chains.append(JudgeTileChain(copy.deepcopy(touch_play_pairs), draw_hands_cur))
+            print(JudgeTileChain(copy.deepcopy(touch_play_pairs), draw_hands_cur))
         for card in range(0, 18):
             if tiles_18[card] < 4:
                 tiles_18[card] += 1
@@ -37,13 +39,17 @@ class VesAI(object):
                     if tiles_18[card1] > 0 and card1 != card:
                         tiles_18[card1] -= 1
                         touch_play_pair = TouchPlayPair(card, card1)
+                        local = copy.deepcopy(touch_play_pairs)
+                        local.append(touch_play_pair)
                         if n == 1:
                             draw_hands = self.calc_draw_hands(tiles_18)
                             if draw_hands:
-                                local = copy.deepcopy(tpps)
-                                local.append(touch_play_pair)
-                                first_layer_jtc.append(JudgeTileChain(local, draw_hands))
+                                judge_tile_chains.append(JudgeTileChain(local, draw_hands))
+                                print(JudgeTileChain(local, draw_hands))
+                        else:
+                            judge_tile_chains.append(
+                                self.calc_effective_cards_internal(tiles_18, n - 1, judge_tile_chains, local))
                         tiles_18[card1] += 1
                 tiles_18[card] -= 1
-        for node in first_layer_jtc:
-            print(node)
+
+        return judge_tile_chains
